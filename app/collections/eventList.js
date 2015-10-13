c6cfa1d1c4d3289153a2c88be425c5ebdd02b772
Nick Kirkes <nkirkes@exacttarget.com>
2015-10-13 11:21:53 -0400

    Increasing search radius because it was driving me nuts.

--- app/collections/eventList.js
+++ app/collections/eventList.js
@@ -18,7 +18,7 @@ app.collections.eventList = Backbone.Collection.extend({
     url: function() {
     	// construct endpoint
     	var endpoint = 'http://api.nytimes.com/svc/events/v2/listings.json?api-key=1b6ec24de1512db2fae47f5165ce39ea%3A6%3A69563513'
-       	+ '&ll=' + this.latitude + ',' + this.longitude;
+       	+ '&ll=' + this.latitude + ',' + this.longitude + '&radius=5000';
 
        	if (this.filters.length > 0) {
 		    endpoint += '&filters=category:(';
